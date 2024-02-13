bcffac37962a9f64f4818ab0cc0452cf7ec6c667
leejoe03 <joelee2003@gmail.com>
2024-02-13 10:51:23 +1000

    working frontend

--- script.js
+++ script.js
@@ -9,10 +9,50 @@ const search = document.getElementById("query");
 
 returnMovies(APILINK)
 function returnMovies(url) {
-    fetch(url).then(res => res.json()).then(function(data)){
+    fetch(url).then(res => res.json()).then(function(data){
         console.log(data.results);
         data.results.forEach(element => {
-            const div = document.createElement('div')
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
+
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
+
+            const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
+            const title = document.createElement('h3');
+            title.setAttribute('class', 'title');
+
+            const center = document.createElement('center');
+
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path; 
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+            main.appendChild(div_row);
+
         });
-    };
+    });
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const serachItem = search.value;
+
+    if (serachItem) {
+        returnMovies(SEARCHAPI + serachItem);
+        search.value = "";
+    }
+})
